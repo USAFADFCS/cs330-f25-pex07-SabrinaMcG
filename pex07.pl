71a65fd95bf54f67c1d276663a929bb355a38d72
SabrinaMcG <110415104+SabrinaMcG@users.noreply.github.com>
2025-11-26 16:07:49 -0800

    Update pex07.pl

--- pex07.pl
+++ pex07.pl
@@ -5,7 +5,76 @@
 %
 % Documentation: None
 %
+cadet(smith).
+cadet(garcia).
+cadet(chen).
+cadet(jones).
 
+fly(ufo).
+fly(weatherBalloon).
+fly(kite).
+fly(fighter).
+fly(cloud).
+
+day(tuesday).
+day(wednesday).
+day(thursday).
+day(friday).
+
+solve :-
+    fly(SmithFly), fly(GarciaFly), fly(ChenFly), fly(JonesFly),
+    all_different([SmithFly, GarciaFly, ChenFly, JonesFly]),
+    
+    day(SmithDay), day(GarciaDay), day(ChenDay), day(JonesDay),
+    all_different([SmithDay, GarciaDay, ChenDay, JonesDay]),
+    
+    Triples = [ [smith, SmithFly, SmithDay],
+                [garcia, GarciaFly, GarciaDay],
+                [chen, ChenFly, ChenDay],
+                [jones, JonesFly, JonesDay] ],
+    
+    % C4C Smith did not see a weather balloon, nor kite.
+    \+ member([smith, weatherBalloon, _], Triples),
+    \+ member([smith, kite, _], Triples),
+    
+    % The one who saw the kite isn’t C4C Garcia.
+    \+ member([garcia, kite, _], Triples),
+    
+    % Friday’s sighting was made by either C4C Chen or the one who saw the fighter aircraft.
+    ( member([chen, _, friday], Triples); member([_, fighter, friday], Triples)),
+    
+    % The kite was not sighted on Tuesday.
+    \+ member([_, kite, tuesday], Triples),
+              
+    % Neither C4C Garcia nor C4C Jones saw the weather balloon.
+    \+ member([garcia, weatherBalloon, _], Triples),
+    \+ member([jones, weatherBalloon, _], Triples),
+    
+    % C4C Jones did not make their sighting on Tuesday.
+    \+ member([jones, _, tuesday], Triples),
+
+    % C4C Smith saw an object that turned out to be a cloud.
+    member([smith, cloud, _], Triples),
+    
+    % The fighter aircraft was spotted on Friday.
+    member([_, fighter, friday], Triples),
+    
+    % The weather balloon was not spotted on Wednesday.
+    \+ member([_, weatherBalloon, wednesday], Triples),
+    
+    tell(smith, SmithFly, SmithDay),
+    tell(garcia, GarciaFly, GarciaDay),
+    tell(chen, ChenFly, ChenDay),
+    tell(jones, JonesFly, JonesDay).
+    
+% Succeeds if all elements of the argument list are bound and different.
+% Fails if any elements are unbound or equal to some other element.
+all_different([H | T]) :- member(H, T), !, fail.
+all_different([_ | T]) :- all_different(T).
+all_different([_]).
+    
+tell(X, Y, Z) :-
+    write('C4C '), write(X), write(' saw the '), write(Y), write(' on '), write(Z), write('.'), nl.
 % The query to get the answer(s) or that there is no answer
 % ?- solve.
 
